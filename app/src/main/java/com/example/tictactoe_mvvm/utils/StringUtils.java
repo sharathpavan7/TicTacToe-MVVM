30ae03ce59eb64a75edb9cbb0f64e14cd01e747a
Sharath Pavan Balla <balla@ivicatechnologies.com>
2020-03-12 18:04:46 +0530

    commit - v0.0.1 view model & StringUtils updated.

--- app/src/main/java/com/example/tictactoe_mvvm/utils/StringUtils.java
+++ app/src/main/java/com/example/tictactoe_mvvm/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.example.tictactoe_mvvm.utils;
 
 public class StringUtils {
 
+    public static String stringFromNumbers(int ... numbers) {
+        StringBuilder builder = new StringBuilder();
+        for (int number : numbers)
+            builder.append(number);
+        return builder.toString();
+    }
+
     public static boolean isNullorEmpty(String value) {
         if (value == null || value.isEmpty()) return true;
         return false;
--- app/src/main/java/com/example/tictactoe_mvvm/viewmodel/GameViewmodel.java
+++ app/src/main/java/com/example/tictactoe_mvvm/viewmodel/GameViewmodel.java
@@ -1,6 +1,7 @@
 package com.example.tictactoe_mvvm.viewmodel;
 
 import android.app.Application;
+import android.util.ArrayMap;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
@@ -8,9 +9,11 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.tictactoe_mvvm.model.Cell;
 import com.example.tictactoe_mvvm.model.Game;
 import com.example.tictactoe_mvvm.model.Player;
+import com.example.tictactoe_mvvm.utils.StringUtils;
 
 public class GameViewmodel extends AndroidViewModel {
 
+    private ArrayMap<String, String> cells;
     private Game game;
 
     public GameViewmodel(Application application) {
@@ -19,11 +22,13 @@ public class GameViewmodel extends AndroidViewModel {
 
     public void init(String player1, String player2) {
         game = new Game(player1, player2);
+        cells = new ArrayMap<>();
     }
 
     public void onClickedCellAt(int row, int column) {
         if (game.cells[row][column] == null) {
             game.cells[row][column] = new Cell(game.currentPlayer);
+            cells.put(StringUtils.stringFromNumbers(row, column), game.currentPlayer.value);
             if (game.hasGameEnded())
                 game.reset();
             else
